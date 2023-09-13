e16221fc742e7323525ec330d0961f094afc73af
lds970311 <ldk2696612575@gmail.com>
2023-09-14 06:40:09 +0800

    重新提交

---
+++ autumn-oa-parent/service-oa/src/main/java/com/autumn/auth/controller/IndexController.java
@@ -0,0 +1,53 @@
+package com.autumn.auth.controller;
+
+import com.autumn.result.Result;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Tag(name = "后台登录管理")
+@RequestMapping("/admin/system/index")
+public class IndexController {
+    /**
+     * 登录
+     *
+     * @return
+     */
+    @PostMapping("login")
+    public Result<Map<String, Object>> login() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", "admin");
+        return Result.ok(map);
+    }
+
+    /**
+     * 获取用户信息
+     *
+     * @return
+     */
+    @GetMapping("info")
+    public Result<Map<String, Object>> info() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("roles", "[admin]");
+        map.put("name", "admin");
+        map.put("avatar", "https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+        return Result.ok(map);
+    }
+
+    /**
+     * 退出
+     *
+     * @return
+     */
+    @PostMapping("logout")
+    public Result<Void> logout() {
+        return Result.ok();
+    }
+
+}
